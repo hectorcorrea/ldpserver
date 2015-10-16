bcd12a98cb6e07a6acbfbd819655a96790b044a3
Hector Correa <hector@hectorcorrea.com>
2015-10-15 20:46:55 -0400

    AST basic structure

--- rdf/ast.go
+++ rdf/ast.go
@@ -1,20 +1,34 @@
 package rdf
 
 import (
-// "errors"
-// "log"
+	// "errors"
+	"log"
 )
 
 type Node struct {
 	value    string
-	children []Node
+	children []*Node
 }
 
-type AST struct {
-	subjects []Node
+func NewNode(value string) Node {
+	return Node{value: value}
 }
 
-func NewAST() AST {
-	var tree AST
-	return tree
+func (node *Node) AddChild(value string) *Node {
+	child := NewNode(value)
+	node.children = append(node.children, &child)
+	return &child
+}
+
+func (subject *Node) Render() string {
+	triples := ""
+	log.Printf("1. s=%s", subject.value)
+	for _, predicate := range subject.children {
+		log.Printf("2. p=%s", predicate.value)
+		for _, object := range predicate.children {
+			log.Printf("3. o=%s", object.value)
+			triples += subject.value + " " + predicate.value + " " + object.value + " .\n"
+		}
+	}
+	return triples
 }
---
+++ rdf/ast_test.go
@@ -0,0 +1,16 @@
+package rdf
+
+import "testing"
+
+func TestNode(t *testing.T) {
+
+	// subject := NewNode("<s>")
+	// predicate := subject.AddChild("<p>")
+	// predicate.AddChild("<o1a>")
+	// predicate.AddChild("<o1b>")
+	// predicate2 := subject.AddChild("<p2>")
+	// predicate2.AddChild("<o2>")
+
+	// t.Errorf("Render\n%s", subject.Render())
+
+}
