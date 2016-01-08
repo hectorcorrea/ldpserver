6c11397f4c562c9819cb4ee311514ad0dbc7e08a
Hector Correa <hector@hectorcorrea.com>
2016-01-08 15:13:03 -0500

    Uses go's flag rather than my custom code to parse args

--- main.go
+++ main.go
@@ -1,43 +1,21 @@
 package main
 
-import "os"
-import "log"
-import "path/filepath"
-import "ldpserver/web"
-import "fmt"
+import (
+	"flag"
+	"ldpserver/web"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	rootFolder, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	rootFolder, err := filepath.Abs(filepath.Dir(os.Args[0]) + "/data")
 	if err != nil {
-		log.Fatal("Could not determine root folder")
-	}
-	address := "localhost:9001"
-	dataPath := rootFolder + "/data"
-	numArgs := len(os.Args)
-
-	if numArgs > 1 {
-		if os.Args[1] == "--help" {
-			showHelp()
-			return
-		}
-
-		address = os.Args[1]
-		if numArgs > 2 {
-			dataPath = os.Args[2]
-		}
+		panic("Could not determine root folder")
 	}
 
-	web.Start(address, dataPath)
-}
+	var address = flag.String("address", "localhost:9001", "Address where server will listen for connections")
+	var dataPath = flag.String("data", rootFolder, "Path where data will be saved")
+	flag.Parse()
 
-func showHelp() {
-	fmt.Printf("Syntax:\n")
-	fmt.Printf("    %s [address] [dataPath]\n", os.Args[0])
-	fmt.Printf("\n")
-	fmt.Printf("address   Represents that address where the server will listen for connections\n")
-	fmt.Printf("          Defaults to localhost:9001\n")
-	fmt.Printf("\n")
-	fmt.Printf("dataPath  Represents the path on disk where data will be saved\n")
-	fmt.Printf("          Defaults to ./data\n")
-	fmt.Printf("\n")
+	web.Start(*address, *dataPath)
 }
